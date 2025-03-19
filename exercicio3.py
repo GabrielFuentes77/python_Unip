b33f0cdc017f308d73b18eba6e8fcc2c8c3c7b40
Gabriel Pinheiro Fuentes Godinho <gabrielfuentesgodinho@gmail.com>
2025-03-18 21:43:33 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,9 @@
+print("Exercício 3 (Teste Par ou Impar) \n")
+
+num = int(input("Informe um número: "))
+
+if(num % 2 == 0):
+    print(f"O número {num} é par!")
+else: 
+    print(f"O número {num} é impar!")
+
