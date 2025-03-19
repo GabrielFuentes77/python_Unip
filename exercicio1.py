1392538fe207523e45d1985fcbfa60e0bae99bf3
Gabriel Pinheiro Fuentes Godinho <gabrielfuentesgodinho@gmail.com>
2025-03-18 21:34:06 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,17 @@
+"""
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+"""
+print("Exercício 1 (soma de 2 numeros) ")
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+soma = n1 + n2
+
+print(f"Resultado da soma: {soma}")
+    
+
