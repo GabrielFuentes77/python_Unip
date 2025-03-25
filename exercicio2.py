d6e5c0b4bb32bbe5c18f1f89416ad45cb4dc0f79
Gabriel Pinheiro Fuentes Godinho <gabrielfuentesgodinho@gmail.com>
2025-03-25 19:58:41 -0300

    Update exercicio2.py
    Adicionando funções

--- exercicio2.py
+++ exercicio2.py
@@ -1,18 +1,12 @@
-"""
+def media(n1,n2,n3):
+    return (n1+n2+n3)/3
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-"""
 print("Exercício 2 (Média) \n")
 
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-n3 = float(input("Digite a terceira nota: "))
-media = (n1 + n2 + n3)/3
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
 
-print(f"Sua média é: {media:.2f}")
+print(f"Sua média é: {media(nota1, nota2, nota3):.2f}")
     
 
