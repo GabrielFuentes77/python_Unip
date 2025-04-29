ef417c76f1577b2a1476b9b8f60120a18b3f80da
Gabriel Pinheiro Fuentes Godinho <gabrielfuentesgodinho@gmail.com>
2025-04-29 12:48:47 -0300

    Update and rename exercicio11.py to trabalho_exercicio11.py

---
+++ trabalho_exercicio11.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+     a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+def main():
+    n = int(input("Digite a quantidade de termos da sequência de Fibonacci: "))
+    if n <= 0:
+        print("Por favor, digite um número maior que zero.")
+    else:
+        print("Sequência de Fibonacci:")
+        fibonacci(n)
+
+if __name__ == "__main__":
+    main()
+    
+
